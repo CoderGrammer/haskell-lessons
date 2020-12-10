0525d55e2fa81ae2db347b189ad310f4edb4a4ba
CoderGrammer <codergrammer@gmail.com>
2020-12-10 21:33:45 +0000

    remove accidental commit

--- out/production/haskell-lessons/Lib.hs
+++
@@ -1,6 +0,0 @@
-module Lib
-    ( aFunction
-    ) where
-
-aFunction :: IO ()
-aFunction = putStrLn "a function..."
--- out/production/haskell-lessons/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-main :: IO ()
-main = print $ safeDivide 10 0
-
--- Maybe consists of Nothing or Just
-safeDivide :: Integral a => a -> a -> Maybe a
-safeDivide a b = 
-  if b == 0 then Nothing else Just $ div a b
